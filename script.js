a33f26fb43b9a00ec1d345c4d10ad84a244098ad
Lalitha <lalitha@Eswaramoorthis-MacBook-Air.local>
2022-01-16 18:21:46 -0500

    Updates made.

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
     });
+    console.log(listedPlanets);
     const faultyList = document.getElementById("faultyItems");
     faultyList.style.visibility = "hidden";
     let form = document.querySelector("#form");
