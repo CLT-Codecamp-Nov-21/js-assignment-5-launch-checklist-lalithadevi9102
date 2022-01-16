2dc5fb832b851529e89df4d2abe0d48282d0ae8b
Lalitha <lalitha@Eswaramoorthis-MacBook-Air.local>
2022-01-16 18:19:30 -0500

    Updates made.

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ window.addEventListener("load", function() {
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
     let listedPlanetsResponse = myFetch();
-    listedPlanetsResponse.then(function (json) {
-        listedPlanets = pickPlanet(json);
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = pickPlanet(result);
         addDestinationInfo(document, listedPlanets["name"], listedPlanets["diameter"], listedPlanets["star"], listedPlanets["distance"], listedPlanets["moons"], listedPlanets["image"]);
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
     });
-    const faultyItemsElement = document.getElementById("faultyItems");
-    faultyItemsElement.style.visibility = "hidden";
+    const faultyList = document.getElementById("faultyItems");
+    faultyList.style.visibility = "hidden";
     let form = document.querySelector("#form");
     const propagation = true;
     form.addEventListener("submit", function(event){
@@ -19,9 +19,9 @@ window.addEventListener("load", function() {
         let copilotName = document.querySelector("div.formField [name=copilotName]").value;
         let fuelLevel = document.querySelector("div.formField [name=fuelLevel]").value;
         let cargoMass = document.querySelector("div.formField [name=cargoMass]").value;
-        if(formSubmission(document, faultyItemsElement, pilotName, copilotName, fuelLevel, cargoMass) === "Empty"){
+        if(formSubmission(document, faultyList, pilotName, copilotName, fuelLevel, cargoMass) === "Empty"){
             alert("All fields are required!");
-        }else if(formSubmission(document, faultyItemsElement, pilotName, copilotName, fuelLevel, cargoMass) === "Invalid"){
+        }else if(formSubmission(document, faultyList, pilotName, copilotName, fuelLevel, cargoMass) === "Invalid"){
             alert("Make sure to enter valid information for each field!");
         }
         event.preventDefault();
