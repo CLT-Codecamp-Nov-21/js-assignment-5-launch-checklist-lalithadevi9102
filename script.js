0b306f45a01275459d8f251bfa61f0113ba9de20
Lalitha <lalitha@Eswaramoorthis-MacBook-Air.local>
2022-01-16 17:50:16 -0500

    Check list completed.

--- script.js
+++ script.js
@@ -4,13 +4,36 @@ window.addEventListener("load", function() {
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
+    listedPlanets = pickPlanet(result);
+    addDestinationInfo(document, listedPlanets["name"], listedPlanets["diameter"], listedPlanets["star"], listedPlanets["distance"], listedPlanets["moons"], listedPlanets["image"]);
+   console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
+
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+
+       let faultyList = document.getElementById('faultyItems');
+    faultyList.style.visibility = "hidden";
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+       
+        //List DOM
+        let pilot = document.querySelector("input[name=pilotName]").value;
+        let coPilot = document.querySelector("input[name=copilotName]").value;
+        let fuel = document.querySelector("input[name=fuelLevel]").value;
+        let cargo = document.querySelector("input[name=cargoMass]").value;
+       
+            formSubmission(document,list,pilot,copilot,fuelLevel,cargoLevel);
+
+        event.preventDefault();
+        if(formSubmission(document, faultyList, pilot, coPilot, fuel, cargo) === "Empty"){
+            alert("All field values are required!");
+        }else if(formSubmission(document, faultyList, pilot, coPilot, fuel, cargo) === "Invalid"){
+            alert("Make sure to enter valid data in each field!");
+        }
+    });    
    
+});
 });
